ec799727eb7ca2f5aa9260435327e77271bd15f2
c14180013 <c14180013@john.petra.ac.id>
2020-04-15 09:58:46 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  name = 'C14180013';
 
   x;
   y;
-  hasilHitung;
+  hasilHitung = 0;
   pola = '';
   
   hitung() {
     this.hasilHitung = Math.pow(this.x, this.y);
+    this.pola = "Hasil: " + this.hasilHitung + "<br/>";
 
     if (this.hasilHitung % 2 != 0) {
-      this.pola = "Pola A";
+      this.pola += "Pola A<br/>"
+
+      
     }
     else
     {
-      this.pola = "Pola B";
+      this.pola = "Pola B<br/>";
+
+      for (let i = this.y; i < this.hasilHitung; i++) {
+        for (let j = 0; j < i; j++) {
+          this.pola += "*";
+        }
+        this.pola += "<br/>";
+      }
     }
+  }
+
+  clear() {
+    this. x= "";
+    this.y = "";
+    this.hasilHitung = 0;
   }
 }
