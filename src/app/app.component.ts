973b4c7ab837e0069ea45f3ca59275f230b4d627
c14180013 <c14180013@john.petra.ac.id>
2020-04-15 10:25:06 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,16 +17,43 @@ export class AppComponent  {
     this.hasilHitung = Math.pow(this.x, this.y);
     this.pola = "Hasil: " + this.hasilHitung + "<br/>";
 
+    let a, b;
+    if (this. x > this.y)
+    {
+      a = this.x;
+      b = this.y;
+    }
+    else
+    {
+      a = this.y;
+      b = this.x;
+    }
+
     if (this.hasilHitung % 2 != 0) {
       this.pola += "Pola A<br/>"
 
-      
+      for (let i = a, j = 1; i > 0 || j <= b; i--, j++) {
+        if (i > 0)
+        {
+          for (let k = 0; k < i; k++) {
+            this.pola += "*";
+          }
+        }
+        
+        if (j <= b)
+        {
+          for (let k = 0; k < j; k++) {
+            this.pola += "o";
+          }
+        }
+        this.pola += "<br/>";
+      }
     }
     else
     {
       this.pola = "Pola B<br/>";
 
-      for (let i = this.y; i < this.hasilHitung; i++) {
+      for (let i = a - 1; i < this.hasilHitung; i++) {
         for (let j = 0; j < i; j++) {
           this.pola += "*";
         }
@@ -39,5 +66,6 @@ export class AppComponent  {
     this. x= "";
     this.y = "";
     this.hasilHitung = 0;
+    this.pola = "";
   }
 }
